31e701c795e2304f0a553a53222b40f6e9bbe273
indra.basak <indra.basak@iovation.com>
2017-05-28 21:37:27 -0700

    Fixed memory leak and added mouse over fucntion.

--- src/js/metricsviewer.js
+++ src/js/metricsviewer.js
@@ -600,11 +600,14 @@
                 this.initialized = true;
             }
 
+            $(this.divId).empty();
+
             //call the metric graphics to create the line chart
             MG.data_graphic({
                 title: this.title,
                 description: this.description,
                 //animate_on_load: true,
+                area: false,
                 data: this.values,
                 width: CHART_WIDTH,
                 height: CHART_HEIGHT,
@@ -619,7 +622,12 @@
                 left: LEFT_MARGIN,
                 bottom: BOTTOM_MARGIN,
                 legend: this.legend,
-                legend_target: this.legendDivId
+                legend_target: this.legendDivId,
+                mouseover: function(d, i) {
+                    var format = d3.timeFormat("%b %d, %Y %H:%M:%S %p");
+                    d3.select(this.divId + ' svg .mg-active-datapoint')
+                        .text(format(d.date));
+                }
             });
         };
 
